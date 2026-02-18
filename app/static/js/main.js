1fb123ef8c969ea615903e84d94c0295c403b153
TheTrappyTeapot <157916154+TheTrappyTeapot@users.noreply.github.com>
2026-02-18 15:53:52 +0000

    Initial Flask app structure and landing page scaffold

---
+++ app/static/js/main.js
@@ -0,0 +1,6 @@
+const btn = document.getElementById("helloBtn");
+const out = document.getElementById("output");
+
+btn.addEventListener("click", () => {
+  out.textContent = "JS is working ✅";
+});
